6767b785c2620ba66444d1d4661ff309c33cd2cf
yuvaphalle <yuva.phalle@gmail.com>
2022-01-30 16:06:53 +0100

    Added Live Chat using Moralis

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Login from '../components/Login'
+import Header from '../components/Header';
 import { useMoralis } from 'react-moralis';
+import Messages from '../components/Messages';
 
 export default function Home() {
-  const { isAuthenticated , logout } = useMoralis();
+  const { isAuthenticated  } = useMoralis();
 
   if (!isAuthenticated) {
     return (
@@ -12,7 +14,7 @@ export default function Home() {
   }
   return (
     
-    <div className=" relative">
+    <div className=" relative h-screen overflow-y-scroll overflow-hidden bg-gradient-to-b from-black to-cyan-700">
     {/* <video autoPlay muted loop id="videoBG" >         
     <source src="/images/vbg2.mp4" type="video/mp4"/>       
     </video> */}
@@ -20,11 +22,23 @@ export default function Home() {
         <title>Meta</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex absolute  w-full items-center justify-center space-y-9">
-           
-      <button onClick={logout} className='glow-on-hover'>Logout</button>
+      {/* <div className="flex absolute  w-full items-center justify-center space-y-9">
+            */}
+
+      <div>
+        <div className="max-w mx-auto">
+              
+        {/*header*/}
+
+        <Header />
+        <Messages />
+
+        {/*main*/}
+
+        </div>     
+      </div>
       
       </div>
-    </div>
+    // </div>
   );
 }
